505a52a53f6b1f8cac9c128dcae6c123af4e587f
sujndoes <sujanhml0@gmail.com>
2026-06-26 07:58:20 +0000

    organized into separate functions

--- shell.c
+++ shell.c
@@ -4,65 +4,109 @@
 #include<unistd.h>
 #include<sys/wait.h>
 
-#define MAX_INPUT 1024
+#define MAX_INPUT 1024 //max user input
+#define MAX_ARGS 64   // max arguments 
+
+void takeInput(char *input)
+{
+    char cwd[MAX_INPUT];
+
+    if (getcwd(cwd,sizeof(cwd)) == NULL)
+    {
+        perror("getcwd failed");
+        exit(1);
+    }
+
+    printf("\nDIR:%s" , cwd);
+    
+    printf(">> ");
+    fflush(stdout);
+
+    if(fgets(input,MAX_INPUT,stdin) == NULL)
+    {
+        exit(1);
+    }
+
+    input[strcspn(input,"\n")] = 0; // remove newline 
+
+}
+
+void parseInput(char *input , char **args)
+{
+    char *token = strtok(input, " ");
+    int argc = 0;
+    while(token!=NULL)
+    {
+        args[argc++] = token;
+        token = strtok(NULL," ");
+    }
+    args[argc] = NULL;
+}
+
+int builtInCommands(char **args) //for handling cd , exit ,help 
+{
+    if(args[0] == NULL)
+    {
+        return 1;
+    }
+    //exit command
+    if(strcmp(args[0],"exit") == 0)
+    {
+        exit(0);
+    }
+    //for changing directory 
+    if(strcmp(args[0],"cd") == 0)
+    {
+        if (args[1] == NULL)
+        {
+            chdir(getenv("HOME"));
+        } else {
+            if(chdir(args[1]) != 0)
+            {
+                perror("cd failed");
+            }
+        }
+        return 1;
+    }
+
+    //for help command
+    if(strcmp(args[0],"help") == 0)
+    {
+        printf("COMING SOON");
+        return 1;
+    }
+    return 0;
+}
 
 int main()
 {
     char input[MAX_INPUT];
-
+    printf("WELCOME TO MY SHELL!!!!\n");
     while(1)
     {
-        printf("myshell> ");
-        fflush(stdout);
-
-        if(fgets(input,MAX_INPUT,stdin) == NULL)
-        {
-            break;
-        }
-
-        input[strcspn(input,"\n")] = 0;
+        
+        takeInput(input);
 
         if(strlen(input) == 0)
         {
             continue;
         }
 
-        char *args[64];
-        int argc = 0;
+        char *args[MAX_ARGS];
 
-        char *token = strtok(input, " ");
-        while(token!=NULL)
-        {
-            args[argc++] = token;
-            token = strtok(NULL," ");
-        }
-        args[argc] = NULL;
+        parseInput(input, args);
 
-        if(strcmp(args[0],"exit") == 0)
+        if(builtInCommands(args) == 1)
         {
-            exit(0);
-        }
-
-        if(strcmp(args[0],"cd") == 0)
-        {
-            if (args[1] == NULL)
-            {
-                chdir(getenv("HOME"));
-            } else {
-                if(chdir(args[1]) != 0)
-                {
-                    perror("cd failed");
-                }
-            }
             continue;
         }
 
-        __pid_t pid = fork();
+        pid_t pid = fork();
 
         if(pid == 0)
         {
             execvp(args[0],args);
-            printf("Command not found: %s\n" , args[0]);
+            perror("command not found!");
             exit(1);
         } else {
             wait(NULL);
