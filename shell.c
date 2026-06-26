0015157873f1831c37c17d55f0ce2f5641112441
sujndoes <sujanhml0@gmail.com>
2026-06-26 12:23:33 +0000

    added pipe support

--- shell.c
+++ shell.c
@@ -78,9 +78,82 @@ int builtInCommands(char **args) //for handling cd , exit ,help
     return 0;
 }
 
+//executing a non piped command
+void executeCommand(char **args)
+{
+    pid_t pid = fork();
+
+    if(pid == 0)
+    {
+        execvp(args[0],args);
+        perror("command not found!");
+        exit(1);
+    } else {
+        wait(NULL);
+    }
+}
+
+//executing piped command 
+void executePiped(char **arg1 , char **arg2)
+{
+    int pipes[2];
+    pipe(pipes);
+
+    pid_t p1 = fork();
+    
+    if(p1 == 0)
+    {
+        dup2(pipes[1] , STDOUT_FILENO);
+        close(pipes[0]); //close read end
+        close(pipes[1]);  
+        execvp(arg1[0],arg1);
+        perror("pipe command 1 failed");
+        exit(1);
+    }
+
+    pid_t p2 = fork();
+    if(p2 == 0)
+    {
+        dup2(pipes[0],STDIN_FILENO);
+        close(pipes[1]);
+        close(pipes[0]);
+        execvp(arg2[0],arg2);
+        perror("pipe command 2 failed");
+        exit(1);
+    }
+
+    close(pipes[0]);
+    close(pipes[1]);
+
+    wait(NULL);
+    wait(NULL);
+}
+
+int parsePipe(char *input,char **left,char **right)
+{
+    char *pipeFound = strchr(input , '|');
+    if(!pipeFound) 
+    {
+        return 1; // pipe doesnt exist // then we execute non piped 
+    }
+    *pipeFound = '\0';
+    pipeFound++;
+
+    while(*pipeFound == ' ') // skip front spaces in right
+    {
+        pipeFound++;
+    }
+
+    *left = input;
+    *right = pipeFound;
+
+    return 0; 
+}
+
 int main()
 {
     char input[MAX_INPUT];
+    
     printf("WELCOME TO MY SHELL!!!!\n");
     while(1)
     {
@@ -92,25 +165,27 @@ int main()
             continue;
         }
 
+        char *left , *right;
+        char *arg1[MAX_ARGS] , *arg2[MAX_ARGS];
         char *args[MAX_ARGS];
 
-        parseInput(input, args);
-
-        if(builtInCommands(args) == 1)
+        if(parsePipe(input,&left,&right) == 0)
         {
-            continue;
-        }
+            parseInput(left , arg1);
+            parseInput(right,arg2);
 
-        pid_t pid = fork();
+            executePiped(arg1,arg2);
+        } else{
+            parseInput(input, args);
 
-        if(pid == 0)
-        {
-            execvp(args[0],args);
-            perror("command not found!");
-            exit(1);
-        } else {
-            wait(NULL);
+            if(builtInCommands(args) == 1)
+            {
+                continue;
+            }
+
+            executeCommand(args);
         }
+        
     }
     return 0;
 }
